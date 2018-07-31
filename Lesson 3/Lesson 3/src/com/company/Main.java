e23637fa6fcd81751180d33bf3c0c4d0bea39950
sergen <bayanav.sergen@icloud.com>
2018-07-31 13:01:38 +0300

    Lesson 3

---
+++ Lesson 3/Lesson 3/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in new Scanner(System.in);
+
+        /*
+        int x = 10;
+        int y = 20;
+        System.out.println(x + y);
+
+        if ( x == y){
+            System.out.println("x == y");
+        }
+
+        int counter = 0;
+        while (counter < 10){
+            System.out.println(counter);
+            continue++;
+        }
+        */
+
+
+        int m = 0;
+        //קלוט מספר למשתנה a
+        System.out.println("enter A");
+        int a = in.nextInt();
+
+        //קלוט מספר למשתנה b
+        System.out.println("enter B:");
+        int b = in.nextInt();
+
+        //עבור k מ a עד b (כולל) בצע
+        for (int k = a; k < b; k++) {
+            //קלוט מספר למשתנה x
+        }
+
+    }
+}
