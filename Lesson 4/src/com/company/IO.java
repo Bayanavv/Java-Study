4fe91bdf6b695645e88decca8623771d7d67d89c
sergen <bayanav.sergen@icloud.com>
2018-07-31 17:16:59 +0300

    Lesson 3

--- Lesson 4/src/com/company/IO.java
+++ Lesson 4/src/com/company/IO.java
@@ -1,7 +1,13 @@
 package com.company;
 
+//קיצור שם
+//Input Out Put
+
+
 public class IO {
+
     public static void myFunc(){
+
         System.out.println("hello, aloha, shalom, nihau");
     }
 }
