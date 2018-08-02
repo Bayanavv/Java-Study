5f4cbef9bf783f06c66b92fafece932c2a7df4ff
sergen <bayanav.sergen@icloud.com>
2018-08-02 19:10:09 +0300

    Lesson 12

---
+++ Lesson 12/src/com/company/utils/IO.java
@@ -0,0 +1,149 @@
+package com.company.utils;
+
+import java.util.Scanner;
+
+
+
+
+/**
+ * created a new class called IO -> new Java Class... IO
+ * Class names Start with Capital Letters!
+ */
+
+//final class can't be inherited.
+public final class IO{
+
+    private static Scanner in = new Scanner(System.in);
+
+    public static int getInt(String message) {
+        boolean isInputValid = false;
+        int input = 0;
+
+        while (!isInputValid){
+            try{
+                System.out.println(message);
+                input = in.nextInt();
+                isInputValid = true; // the input is valid
+            }
+            catch(Exception e){
+                in.next();//remove the string from the input
+                System.out.println("try again");
+            }
+        }
+        return input;
+    }
+
+    public static String getString(String message) {
+        System.out.println(message);
+        return in.next();
+    }
+
+    public static double getDouble(String message) {
+        boolean isInputValid = false;
+        double input = 0;
+
+        while (!isInputValid){
+            try{
+                System.out.println(message);
+                input = in.nextDouble();
+                isInputValid = true; // the input is valid
+            }
+            catch(Exception e){
+                in.next();//remove the string from the input
+                System.out.println("try again");
+            }
+        }
+        return input;
+    }
+
+    public static boolean getBoolean(String message) {
+        boolean isInputValid = false;
+        boolean input = false;
+
+        while (!isInputValid){
+            try{
+                System.out.println(message);
+                input = in.nextBoolean();
+                isInputValid = true; // the input is valid
+            }
+            catch(Exception e){
+                in.next();//remove the string from the input
+                System.out.println("try again");
+            }
+        }
+        return input;
+    }
+
+    public static float getFloat(String message) {
+        boolean isInputValid = false;
+        float input = 0;
+
+        while (!isInputValid){
+            try{
+                System.out.println(message);
+                input = in.nextFloat();
+                isInputValid = true; // the input is valid
+            }
+            catch(Exception e){
+                in.next();//remove the string from the input
+                System.out.println("try again");
+            }
+        }
+        return input;
+    }
+
+    public static int[][] getMultTable() {
+        int n = IO.getInt("Table size?");
+
+        int[][] table = new int[n][n];//{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+
+    public static int[] getIntArray() {
+        //קלוט מס' שלם - n
+        int n = IO.getInt("How many items?");
+        //ניצור מערך מגודל n
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = IO.getInt("Enter the next item:");
+        }
+
+        return arr;
+    }
+
+    public static double getAvg(int[] arr) {
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum / arr.length;
+    }
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            //System.out.print(arr[i] + " "); //print in one line.
+
+            System.out.printf("%4d", arr[i]);
+            //printf -> smarter than print
+        }
+        //new line()
+        System.out.println();
+    }
+
+    public static void print(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            IO.print(arr[i]);
+        }
+    }
+
+
+
+
+}
--- Lesson 4/src/com/company/IO.java
+++ Lesson 4/src/com/company/IO.java
@@ -4,10 +4,25 @@ package com.company;
 //Input Out Put
 
 
+import java.util.Scanner;
+
 public class IO {
 
     public static void myFunc(){
 
         System.out.println("hello, aloha, shalom, nihau");
     }
+    public static void targil(){
+
+        System.out.println("targil");
+    }
+
+    static void getAge(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter your age: ");
+        int age = in.nextInt();
+
+    }
+
+
 }
