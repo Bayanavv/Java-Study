56bc2e7c12fea1e53a87ec6cb23fa6795c2dcc03
sergen <bayanav.sergen@icloud.com>
2018-08-16 21:08:45 +0300

    Lessopn 14

---
+++ Lesson 14/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.company.utils.IO;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 0; i < 10; i++) {
+            int r = IO.getRandom(10);
+            System.out.println(10);
+        }
+    }
+}
