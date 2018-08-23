c4d23e59e99594a1240074c100ab5b341684e837
sergen <bayanav.sergen@icloud.com>
2018-08-23 20:05:53 +0300

    Lesson 1

---
+++ Lesson 18/src/com/company/TrivialQuiz.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.json.simple.JSONObject;
+
+public class TrivialQuiz {
+    //Properties:
+    private List<TrivialQuestion>questions;
+    private int score =0;
+
+    public int score = 0;
+    public JSONObject toJson(){
+        //JsonArray qs = new...
+        //fill the array with questions.toJason()
+
+        //JSONObject obj = new...
+        //obj.put*(score
+        //obj.put*(questions,qs
+    }
+}
