8ac05ab39f5f02770cfa679d69dde7d325167e98
sergen <bayanav.sergen@icloud.com>
2019-04-06 18:25:07 +0300

    Java Exercise 1

---
+++ src/com/bayanavv/Dog.java
@@ -0,0 +1,22 @@
+package com.bayanavv;
+
+public class Dog extends Animal{
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(int size, int weight, String name,int eyes, int legs, int tail,  int teeth, String coat) {
+        super(1, 1, size, weight, name);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+
+    }
+
+
+}
